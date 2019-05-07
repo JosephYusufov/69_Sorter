535fd267984bc29a584e8d080df71dba28b7f3d2
JosephYusufov <jyusufov00@stuy.edu>
2019-05-06 23:33:31 -0400

    succesfully implemented InsertionSort using the Sorter inheritance structure. Now onto SelectionSort

--- InsertionSorter.java
+++ InsertionSorter.java
@@ -1,23 +1,54 @@
+/*******************************************************************************
+CREDIT FOR THE insertionSort AND insert1 METHODS GO TO SI LIANG LEI
+*******************************************************************************/
+
 /**
   Implement an insertion sort, in the Sorters structure
  */
 import java.util.ArrayList;
 
 public class InsertionSorter extends Sorter {
-
-
-    /**
-      Construct an instance to process the user's data
-     */
-    public InsertionSorter(  ArrayList< String> usersData) {
-        // your code here, if necessary
+    public InsertionSorter(ArrayList<String> list){
+        super(list);
     }
-    
+    public static void insertionSort(ArrayList<String> list) {
+        for(int elementToInsert = 1;
+            elementToInsert < list.size();
+            insert1(list,elementToInsert),
+            elementToInsert++
+            ) {
+                // System.out.println("    dbg: "
+                    // + "after inserting element " + elementToInsert
+                    // + " elements: " + list
+                    // );
+            }
+    }
 
-    /**
-      sort the user's data, implementing insertion sort
-     */
+    private static void insert1(ArrayList<String> list, int numSorted) {
+        String valueToInsert = list.get(numSorted);
+        boolean inserted = false;
+        for(int indexToCompare = numSorted - 1;
+            !inserted && indexToCompare > -1;
+            indexToCompare--
+            ) {
+                if(indexToCompare == 0 && valueToInsert.compareTo(list.get(0)) < 0) {
+                    list.set(1, list.get(0));
+                    list.set(0, valueToInsert);
+                    inserted = true;
+                }
+                else {
+                    if (valueToInsert.compareTo(list.get(indexToCompare)) < 0) {
+                        list.set(indexToCompare+1, list.get(indexToCompare));
+                    }
+                    else {
+                        list.set(indexToCompare+1, valueToInsert);
+                        inserted = true;
+                    }
+                }
+        }
+    }
+
     public void mySort() {
-        // your code here
+        insertionSort(elements);
     }
 }
